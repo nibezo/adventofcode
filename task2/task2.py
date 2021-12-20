ce6978574cab2fe548eafeca212fff0ed6055ead
nibezo <nibezo.cs@gmail.com>
2021-12-20 23:02:56 +0300

    feat: add task2 solution

---
+++ task2/task2.py
@@ -0,0 +1,24 @@
+# https://adventofcode.com/2021/day/2
+
+data = open('input2.txt', 'r')
+
+# convert the file to the list "nums" line by line
+data = data.readlines()
+
+# list with all commands
+commands = []
+horizontal_position, depth = 0, 0
+
+# delete \n
+for line in data:
+    commands.append(line.strip())
+
+for command in commands:
+    if command[0:4] == "down":
+        depth += int(command[-1])
+    elif command[0:2] == "up":
+        depth -= int(command[-1])
+    elif command[0:7] == "forward":
+        horizontal_position += int(command[-1])
+
+print(f"Result is {horizontal_position * depth}")
