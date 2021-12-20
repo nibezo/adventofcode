5518b98e7a710daa2ba56220806d02edc88695d9
nibezo <nibezo.cs@gmail.com>
2021-12-21 00:12:58 +0300

    feat: add task3 solution

--- task3/task3.py
+++ task3/task3.py
@@ -1,24 +1,44 @@
+# https://adventofcode.com/2021/day/3
 data = open('input3.txt', 'r')
 
 # convert the file to the list "binaries" line by line
 data = data.readlines()
 
 # list with all
-binaries = []
-i, result = 0, 0
-gamma_rate, epsilon_rate = '0', '0'
+binaries_old = []
+binaries_new = []
+result, row = 0, ''
+gamma_rate, epsilon_rate = '', ''
 
 # delete \n
 for line in data:
-    binaries.append(line.strip())
+    binaries_old.append(line.strip())
 
-for rows in binaries:
-    for column in binaries:
-        pass
+# replace rows to columns
+for i in range(0, 12):
+    for bit in binaries_old:
+        row += bit[i]
+    binaries_new.append(row)
+    row = ''
 
 
-gamma_rate = int(gamma_rate)
-epsilon_rate = int(epsilon_rate)
+for i in range(0, 12):
+    # count the number of zero bits and one bits
+    zero = binaries_new[i].count('0')
+    one = binaries_new[i].count('1')
+
+    # create gamma_rate and epsilon_rate
+    # gamma_rate is the most used "1" bits
+    # epsilon_rate is the most used "0" bits
+    if one > zero:
+        gamma_rate += '1'
+        epsilon_rate += '0'
+    else:
+        gamma_rate += '0'
+        epsilon_rate += '1'
+
+# convert binary to decimal with int(x, 2) function
+gamma_rate = int(gamma_rate, 2)
+epsilon_rate = int(epsilon_rate, 2)
 
 print(f"Result is {gamma_rate * epsilon_rate}")
-print(len(binaries[1]))
